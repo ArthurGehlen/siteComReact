e875692c11df28ef3981de63326e9fe360383c53
Arthur Gehlen <1arthurgehlen@gmail.com>
2024-10-28 22:48:34 -0300

    Commit Inicial.

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+function HelloWorld() {
+    return (
+        <div className='HelloWorld'>
+            <h2>
+                Hello, World!
+            </h2>
+        </div>
+    )
+}
+
+export default HelloWorld

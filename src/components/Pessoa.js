cae6d698a2fca8e51d33d79ab021a96ef3031344
Arthur Gehlen <1arthurgehlen@gmail.com>
2024-12-13 22:16:07 -0300

    Css

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,10 +1,12 @@
+import './Pessoa.module.css';
+
 function Pessoa({ name, age, avatar }) {
 
     return (
         <div>
             <img src={avatar} alt={name} />
             <h2>Nome: {name}</h2>
-            <p>Idade: {age}</p>
+            <p>age: {age}</p>
         </div>
     )
 

f51118c2bf26a780c7b7bc58367ad8537bfc616a
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-07 22:21:01 -0300

    Renderização de listas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
+import OutraLista from './components/Listas/OutraLista';
 // import HelloWorld from './components/HelloWorld'
 // import SayMyName from './components/SayMyName'
 // import Pessoa from './components/Pessoa'
 // import List from './components/List'
-import Evento from './components/Evento'
-import Form from './components/Form';
-import Condicional from './components/Condicional';
+// import Evento from './components/Evento'
+// import Form from './components/Form';
+// import Condicional from './components/Condicional';
 
 function App() {
 
+  const itens = ["React", "Vue", "Angular"]
+
   return (
     <div className="App">
-      {/* <HelloWorld /> */}
-      {/* <SayMyName name={name} /> */}
-
       {/* <Pessoa
         name="Arthur"
         age="15"
@@ -26,7 +26,11 @@ function App() {
       {/* <Evento />
       <Form /> */}
 
-      <Condicional />
+      {/* <Condicional /> */}
+
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={itens} />
+      <OutraLista itens={[]} />
     </div>
   );
 

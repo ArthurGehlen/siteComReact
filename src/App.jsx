e4c4ddc44a99704622edae48b7f17631b4bd7e21
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-01-10 22:19:24 -0300

    React Icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // import { useState } from 'react';
 // import OutraLista from './components/Listas/OutraLista';
-// import HelloWorld from './components/HelloWorld'
-// import SayMyName from './components/SayMyName'
-// import Pessoa from './components/Pessoa'
-// import List from './components/List'
 // import Evento from './components/Evento'
 // import Form from './components/Form';
 // import Condicional from './components/Condicional';
 // import SeuNome from './components/SeuNome';
 // import Saudacao from './components/Saudacao';
-import NavBar from "./components/Layout/NavBar"
+import NavBar from "./components/Layout/NavBar/NavBar"
 import Home from "./pages/Home"
 import Empresa from "./pages/Empresa"
 import Contato from "./pages/Contato"
-import Footer from "./components/Layout/Footer"
+import Footer from "./components/Layout/Footer/Footer"
 
 function App() {
 
@@ -24,12 +20,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Pessoa
-        name="Arthur"
-        age="15"
-        avatar="https://placehold.co/150" />
-    
-      <List /> */}
       {/* <h1>Testando Eventos</h1> */}
 
       {/* <Evento number={1} /> */}
